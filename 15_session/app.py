8e49aecd23a49cc2d03d27ff6424beeac3d5b0bb
Andrew <andrewjuang01@gmail.com>
2021-10-19 22:31:14 -0400

    update notes + tryexcept

--- 15_session/app.py
+++ 15_session/app.py
@@ -52,8 +52,11 @@ def authenticate():
 def logout():
     ''' Logout user by deleting user from session dict. Redirects to loginpage'''
 
-    # Delete user
-    session.pop('username')
+    # Delete user. This try... except... block prevent an error from ocurring when the logout page is accessed from the login page
+    try:
+        session.pop('username')
+    except KeyError:
+        return redirect(url_for('disp_loginpage'))
     # Redirect to login page
     return redirect(url_for('disp_loginpage'))
 
