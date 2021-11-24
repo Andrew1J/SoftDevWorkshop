d82c7f16a6e1995aeed68a50c761fcc382de88dd
Andrew <andrewjuang01@gmail.com>
2021-11-23 21:13:08 -0500

    added urllib way

--- 19_api/app.py
+++ 19_api/app.py
@@ -4,7 +4,7 @@
 # 2021-11-23
 
 from flask import Flask, render_template
-import requests
+import requests, urllib, json
 
 app = Flask(__name__)
 
@@ -13,14 +13,23 @@ API_KEY = f.read().strip()
 
 @app.route("/")
 def main():
-    print("API_KEY: " + API_KEY + "\n")
-    r = requests.get('https://api.nasa.gov/planetary/apod?api_key=' + API_KEY)
+    # Using urllib library
+    r = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=' + API_KEY)
 
-    print("JSON:")
-    print(r.json())
+    JSON = r.read()
 
-    URL = r.json()['url']
-    print("\nURL: " + URL)
+    URL = json.loads(JSON)['url']
+
+
+    # Using requests library
+    # print("API_KEY: " + API_KEY + "\n")
+    # r = requests.get('https://api.nasa.gov/planetary/apod?api_key=' + API_KEY)
+    #
+    # print("JSON:")
+    # print(r.json())
+    #
+    # URL = r.json()['url']
+    # print("\nURL: " + URL)
 
     return render_template('main.html',pic=URL)
 
